064b5be6e08b7bfacd36275b59bf155f843cb161
haikalnuril <haikal.nuril23@gmail.com>
2024-11-07 18:47:09 +0700

    feat: use state

--- ch5-backend-shop-rest-api/controllers/authController.js
+++ ch5-backend-shop-rest-api/controllers/authController.js
@@ -1,5 +1,5 @@
 const { Auths, Users } = require("../models");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const register = async (req, res, next) => {
@@ -12,68 +12,67 @@ const register = async (req, res, next) => {
 };
 
 const login = async (req, res, next) => {
-  try {
-      const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-      // Find user by email
-      const data = await Auths.findOne({
-          where: {
-              email,
-          },
-          include: [
-              {
-                  model: Users,
-                  as: "user",
-              },
-          ],
-      });
+        // Find user by email
+        const data = await Auths.findOne({
+            where: {
+                email,
+            },
+            include: [
+                {
+                    model: Users,
+                    as: "user",
+                },
+            ],
+        });
 
-      if (!data) {
-          return res.status(404).json({
-              status: "Failed",
-              message: "User not found",
-              isSuccess: false,
-              data: null,
-          });
-      }
-      if (data && bcrypt.compareSync(password, data.password)) {
+        if (!data) {
+            return res.status(404).json({
+                status: "Failed",
+                message: "User not found",
+                isSuccess: false,
+                data: null,
+            });
+        }
+        if (data && bcrypt.compareSync(password, data.password)) {
             const token = jwt.sign(
-              {
-                id: data.id,
-                username: data.user.username,
-                email: data.email,
-                userId: data.user.id,
-              },
-              process.env.JWT_SECRET,
-              {
-                expiresIn: '1d',
-              }
+                {
+                    id: data.id,
+                    username: data.user.username,
+                    email: data.email,
+                    userId: data.user.id,
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "1d",
+                }
             );
 
-          return res.status(200).json({
-              status: "Success",
-              message: "Berhasil login",
-              data: {
-                  token,
-                  user: data,
-              },
-          });
-      } else {
-          return res.status(401).json({
-              status: "Failed",
-              message: "Username or Password is Invalid",
-              isSuccess: false,
-              data: null,
-          });
-      }
-
-  } catch (err) {
-      console.error(err);
-      res.status(500).json({
-          status: "Failed",
-          message: err.message,
-      });
-  }
+            return res.status(200).json({
+                status: "Success",
+                message: "Berhasil login",
+                data: {
+                    token,
+                    user: data,
+                },
+            });
+        } else {
+            return res.status(401).json({
+                status: "Failed",
+                message: "Username or Password is Invalid",
+                isSuccess: false,
+                data: null,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: "Failed",
+            message: err.message,
+        });
+    }
 };
 const authenticate = async (req, res) => {
     try {
